29ff9066709c535336c0747e46b7469c1cddef90
James Thompson <qbi11y@gmail.com>
2015-09-22 10:18:18 -0500

    Added coming soon sections to the content and nav

---
+++ templates/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GravitantDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GravitantDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("156541fc-c8a7-4b3c-a98c-5bede386b024")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ templates/js/toolkitApp.js
@@ -0,0 +1,105 @@
+﻿var toolkitApp = angular.module('toolkitApp', ['smart-table']);
+
+toolkitApp.run(function ($rootScope) {
+    $rootScope.data = {
+        theme: 'gravitant-toolkit'
+    };
+
+    $('body').tooltip({
+        selector: '[data-toggle="tooltip"]',
+        container: 'body'
+    });    
+
+    $('body').popover({
+        selector: '[data-toggle="popover"]',
+        container: 'body',
+        html: true,
+        content: function () {
+            return $(this).next('.popover-content').html();
+        }         
+    });
+    
+    $('.popper').popover({
+        container: 'body',
+        html: true,
+       
+    });
+    
+    $('body').on('click', function (e) {
+        $('[data-toggle="popover"]').each(function () {
+            //the 'is' for buttons that trigger popups
+            //the 'has' for icons within a button that triggers a popup
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                $(this).popover('hide');
+            }
+        });
+    });
+    
+});
+
+
+toolkitApp.controller('MainController',
+    ['$scope',
+        function ($scope) {
+
+            $scope.data = {
+                filter1: null,
+                filter2: null,
+                filterItems: [],
+                listItems: [],
+                checkAll: false
+            };
+
+            $scope.viewmodel = {};
+
+            $scope.actions = {
+                removeFilter: function(filter) {
+                    if (filter.filter == 'Field 1') $scope.data.filter1 = null;
+                    if (filter.filter == 'Field 2') $scope.data.filter2 = null;
+                },
+                clearFilters: function() {
+                    $scope.data.filter1 = null;
+                    $scope.data.filter2 = null;
+                    $scope.actions.updateFilter();
+                },
+                updateFilter: function () {
+                    $scope.data.filterItems = [];
+                    if ($scope.data.filter1) { $scope.data.filterItems.push({ filter: 'Field 1', filterValue: $scope.data.filter1 }); }
+                    if ($scope.data.filter2) { $scope.data.filterItems.push({ filter: 'Field 2', filterValue: $scope.data.filter2 }); }
+                }
+            };           
+
+            $scope.$watch('data.checkAll', function (val) {
+                angular.forEach($scope.data.listItems, function (li) {
+                    li.selected = val;
+                });
+            });
+
+            $scope.$watch('data.filter1', function (val) {
+                $scope.actions.updateFilter();
+            });
+
+            $scope.$watch('data.filter2', function (val) {
+                $scope.actions.updateFilter();
+            });
+            
+            $scope.initialize = function () {
+                for (var i = 1; i <= 5; i++) {
+                    $scope.data.listItems.push(
+                        {
+                            name: "List Item " + i,
+                            selected: false,
+                            column1: 'Value ' + i,
+                            column2: 'Value ' + i,
+                            column3: 'Value ' + i,
+                            column4: 'Value ' + i
+                        });
+                }
+
+                $scope.actions.updateFilter();
+            };
+
+            $scope.initialize();
+
+        }])
+;
